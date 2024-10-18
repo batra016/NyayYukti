a7f567c59e8d775e03f272238364690044f24379
batra016 <yuktibatra016@gmail.com>
2024-10-18 21:34:24 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import ButtonGradient from './assets/svg/ButtonGradient'
+import Header from './components/Header'
+
+
+function App() {
+
+  return (
+    <>
+      <div className='pt-[4.75rem] lg:pt-[5.75rem] overflow-hidden'><Header /></div>
+      <ButtonGradient></ButtonGradient>
+
+    </>
+  )
+}
+
+export default App
