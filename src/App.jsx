0229cecf3e31c7b9385f60c0c622536180773221
batra016 <yuktibatra016@gmail.com>
2024-10-30 00:40:32 +0530

    hero section

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@
 import ButtonGradient from './assets/svg/ButtonGradient'
 import Header from './components/Header'
 
+import Hero from './components/Hero'
 
 function App() {
 
   return (
     <>
-      <div className='pt-[4.75rem] lg:pt-[5.75rem] overflow-hidden'><Header /></div>
+      <div className='pt-[4.75rem] lg:pt-[5.75rem] overflow-hidden'>
+        <Header />
+        <Hero />
+      </div>
       <ButtonGradient></ButtonGradient>
 
     </>
